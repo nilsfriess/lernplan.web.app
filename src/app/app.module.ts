7e5473eb3d3fe2a7c14ff4513ae086c3a979e5f7
mariusfriess <friess.marius@gmail.com>
2019-09-18 18:05:23 +0200

    comp template generation + prettier integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,34 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './comp/auth/login/login.component';
+import { RegisterComponent } from './comp/auth/register/register.component';
+import { TasksComponent } from './comp/page/tasks/tasks.component';
+import { AccountComponent } from './comp/page/account/account.component';
+import { NewTaskComponent } from './comp/page/tasks/new-task/new-task.component';
+import { NavComponent } from './comp/nav/nav.component';
+import { BottomNavComponent } from './comp/nav/bottom-nav/bottom-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    TasksComponent,
+    AccountComponent,
+    NewTaskComponent,
+    NavComponent,
+    BottomNavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    BrowserAnimationsModule
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/comp/page/tasks/new-task/new-task.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lp-new-task',
+  templateUrl: './new-task.component.html',
+  styleUrls: ['./new-task.component.scss'],
+})
+export class NewTaskComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
